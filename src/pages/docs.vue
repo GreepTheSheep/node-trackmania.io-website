696398a9ed75384b1b0df28034451917ed129d5a
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-10 14:25:28 +0100

    removing useless files from imports

--- src/pages/docs.vue
+++ src/pages/docs.vue
@@ -21,11 +21,7 @@ import { whenever, useEventListener, useMagicKeys } from '@vueuse/core';
 
 import { useStore } from '~/store';
 import MainSource from '~/data/MainSource';
-import CollectionSource from '~/data/CollectionSource';
 import BuildersSource from '~/data/BuildersSource';
-import VoiceSource from '~/data/VoiceSource';
-import CommandoSource from '~/data/CommandoSource';
-import RPCSource from '~/data/RPCSource';
 import { fetchError } from '~/util/fetchError';
 
 import Sidebar from '~/components/Sidebar.vue';
