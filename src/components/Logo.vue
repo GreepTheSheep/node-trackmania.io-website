3648cdecd882e071521d5be5efcdada797609858
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-10 20:42:21 +0100

    Adjusting svg for mobile device

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -3,8 +3,8 @@
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
 		version="1.1"
-		width="700"
-		height="170"
+		width="100%"
+		height="100%"
 		viewBox="0 0 700 170"
 		xml:space="preserve"
 	>
