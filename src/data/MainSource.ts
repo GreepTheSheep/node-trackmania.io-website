da40df3649b44c10475d296a5b6738d54627b655
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-18 23:42:25 +0100

    remove depfu branches

--- src/data/MainSource.ts
+++ src/data/MainSource.ts
@@ -9,6 +9,7 @@ export default new DocsSource({
 	global: 'TMIO',
 	repo: 'GreepTheSheep/node-trackmania.io',
 	defaultTag: 'main',
-	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+	branchFilter: (branch: string) =>
+		!branchBlacklist.has(branch) && !branch.startsWith('dependabot/') && !branch.startsWith('depfu/'),
 	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '9.0.0'),
 });
