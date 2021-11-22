a7e4250ef8fcc45eb163d845a1e979d5e31ab8f9
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-22 08:33:29 +0100

    Remove tag filter with "v"

--- src/data/MainSource.ts
+++ src/data/MainSource.ts
@@ -11,5 +11,5 @@ export default new DocsSource({
 	defaultTag: 'main',
 	branchFilter: (branch: string) =>
 		!branchBlacklist.has(branch) && !branch.startsWith('dependabot/') && !branch.startsWith('depfu/'),
-	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '9.0.0'),
+	// tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '9.0.0'),
 });
