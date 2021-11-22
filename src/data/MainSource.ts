8c827ab02094de66ad35bfaff0accf649a5b8af2
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-22 08:42:48 +0100

    Tag filter starting with v3

--- src/data/MainSource.ts
+++ src/data/MainSource.ts
@@ -1,3 +1,5 @@
+import semver from 'semver';
+
 import DocsSource from './DocsSource';
 
 const branchBlacklist = new Set(['docs']);
@@ -9,5 +11,5 @@ export default new DocsSource({
 	defaultTag: 'main',
 	branchFilter: (branch: string) =>
 		!branchBlacklist.has(branch) && !branch.startsWith('dependabot/') && !branch.startsWith('depfu/'),
-	// tagFilter: (tag: string) => tag.replace(/^v/, ''),
+	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '3.0.0'),
 });
