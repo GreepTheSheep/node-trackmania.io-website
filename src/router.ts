1bd0577299717b2e5fa14e5410df23828d5ba1ad
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2023-05-23 08:37:22 +0200

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import routes from 'virtual:generated-pages';
+import routes as generated from 'virtual:generated-pages';
 
 export default createRouter({
 	history: createWebHashHistory(),
 	routes: [
-		...routes,
+		...generated,
 		{
 			path: "/discord-bot",
 			name: "Discord Bot",
