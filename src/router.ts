e34473e4688b70ed48515ba5bbe9b1fd187b885d
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2023-05-23 08:33:16 +0200

    Added Discord bot link

--- src/router.ts
+++ src/router.ts
@@ -3,5 +3,14 @@ import routes from 'virtual:generated-pages';
 
 export default createRouter({
 	history: createWebHashHistory(),
-	routes,
+	routes: [
+		...routes,
+		{
+			path: "/discord-bot",
+			name: "Discord Bot",
+			beforeEnter() {
+				window.location.href = "https://discord.com/oauth2/authorize?client_id=810191349214871583&permissions=397888841808&scope=bot%20applications.commands";
+			}
+		}
+	],
 });
